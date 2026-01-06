4574aec717deabcd99b0e3a5a0114daaf5289dc5
Pedro <pedrox90w@gmail.com>
2026-01-06 19:59:21 -0300

    refactor: integrate window finding into CookieVision and enhance clicking logic
    refatoração: integrando window finder na classe CookieVision e modificando variáveis para rodarem ao chamara classe.

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 import time
 from datetime import datetime
-from src.finder.window_finder import encontrar_janela_cookie
 from src.action.clicker import clicar_no_biscoito
 from src.vision.cookie_vision import CookieVision
 
-ENABLE_GOLDEN_COOKIE = True
-ENABLE_UPGRADES      = True
-ENABLE_STORE         = True
-ENABLE_CLICKING      = True
-DEBUG_MODE           = False
+ENABLE_GOLDEN_COOKIE   = True
+ENABLE_UPGRADES        = True
+ENABLE_STORE           = True
+ENABLE_CLICKING        = True
+ENABLE_SUGAR_CLICKING  = True
+ENABLE_POP_UP_KILLER   = True
+DEBUG_MODE             = True
 
 PERC_X = 0.20
 PERC_Y = 0.45
+SUGAR_PERC_X = 0.307
+SUGAR_PERC_Y = 0.1
 INTERVALO_VISAO = 1.0
 INTERVALO_LOJA = 5.0
+INTERVALO_SUGAR = 3600
+INTERVALO_POP_UP_KILLER = 300
 
 def main():
     inicio = datetime.now()
-    hwnd_jogo = encontrar_janela_cookie()
-    if not hwnd_jogo:
-        print("Erro: Janela do Cookie Clicker não encontrada.")
-        return
 
     vision = CookieVision(debug=DEBUG_MODE)
+
     ultima_verificacao_visao = 0
     ultima_verificacao_loja = 0
+    ultima_verificacao_sugar = 0
 
     qtd_golden_cookies = 0
     qtd_upgrades = 0
     qtd_loja = 0
-    
-    rect = vision.get_window_rect()
-    x_dinamico = int(rect["width"] * PERC_X)
-    y_dinamico = int(rect["height"] * PERC_Y)
+
+    if vision.rect:
+        print(f"Janela do jogo encontrada em: {vision.rect}")
+    else:
+        print("Janela do jogo não encontrada.")
+        return
+
+    x_dinamico = int(vision.rect["width"] * PERC_X)
+    y_dinamico = int(vision.rect["height"] * PERC_Y)
+
+    x_dinamico_sugar = int(vision.rect["width"] * SUGAR_PERC_X)
+    y_dinamico_sugar = int(vision.rect["height"] * SUGAR_PERC_Y)
 
     print("Bot iniciado! Pressione Ctrl+C para parar.")
     print(f"Bot iniciado! Flags: Golden:{ENABLE_GOLDEN_COOKIE}, Upgrades:{ENABLE_UPGRADES}, Loja:{ENABLE_STORE}, Cliques:{ENABLE_CLICKING}")
@@ -47,7 +58,7 @@ def main():
                     ponto_golden = vision.find_any_golden()
                     if ponto_golden:
                         print(f"[{time.strftime('%H:%M:%S')}] Golden Cookie!")
-                        clicar_no_biscoito(hwnd_jogo, ponto_golden[0], ponto_golden[1])
+                        clicar_no_biscoito(vision.hwnd, ponto_golden[0], ponto_golden[1])
                         qtd_golden_cookies += 1
 
                     ultima_verificacao_visao = tempo_atual
@@ -55,36 +66,41 @@ def main():
             # Lógica da Loja
             if tempo_atual - ultima_verificacao_loja >= INTERVALO_LOJA:
                 if ENABLE_UPGRADES:    
-                    if rect:
-                        # PRIORIDADE 1
-                        ponto_upgrade = vision.get_upgrade_status(rect)
-                        if ponto_upgrade:
-                            print(f"[{time.strftime('%H:%M:%S')}] Comprei Upgrade")
-                            clicar_no_biscoito(hwnd_jogo, ponto_upgrade[0], ponto_upgrade[1])
-                            time.sleep(0.5)
-                            qtd_upgrades += 1
+                    # PRIORIDADE 1
+                    ponto_upgrade = vision.get_upgrade_status()
+                    if ponto_upgrade:
+                        print(f"[{time.strftime('%H:%M:%S')}] Comprei Upgrade")
+                        clicar_no_biscoito(vision.hwnd, ponto_upgrade[0], ponto_upgrade[1])
+                        time.sleep(0.5)
+                        qtd_upgrades += 1
 
                     # PRIORIDADE 2
                     if ENABLE_STORE:
-                        itens_disponiveis = vision.get_store_status(rect)
+                        itens_disponiveis = vision.get_store_status()
                         if itens_disponiveis:
                             # Clica no urtimo
                             alvo_compra = itens_disponiveis[-1]
                             print(f"[{time.strftime('%H:%M:%S')}] Comprei estrutura da loja")
-                            clicar_no_biscoito(hwnd_jogo, alvo_compra[0], alvo_compra[1])
+                            clicar_no_biscoito(vision.hwnd, alvo_compra[0], alvo_compra[1])
                             qtd_loja += 1
 
                 ultima_verificacao_loja = tempo_atual
 
+            # Cliques no açúcar
+            if tempo_atual - ultima_verificacao_sugar >= INTERVALO_SUGAR:
+                if ENABLE_SUGAR_CLICKING:
+                    clicar_no_biscoito(vision.hwnd, x_dinamico_sugar, y_dinamico_sugar)
+                    print(f"local do click açucar: {x_dinamico_sugar}, {y_dinamico_sugar}")
+
+                ultima_verificacao_sugar = tempo_atual
+
             # Cliques no biscoito principal
             if ENABLE_CLICKING:
                 for _ in range(10):
-                    clicar_no_biscoito(hwnd_jogo, x_dinamico, y_dinamico)
+                    clicar_no_biscoito(vision.hwnd, x_dinamico, y_dinamico)
 
-            rect = vision.get_window_rect()
-            if rect:
-                x_dinamico = int(rect["width"] * PERC_X)
-                y_dinamico = int(rect["height"] * PERC_Y)
+            x_dinamico = int(vision.rect["width"] * PERC_X)
+            y_dinamico = int(vision.rect["height"] * PERC_Y)
             
             # Anti derretimento da CPU
             time.sleep(0.01)
--- src/vision/cookie_vision.py
+++ src/vision/cookie_vision.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from mss import mss
 import win32gui
-from src.finder.window_finder import encontrar_janela_cookie
 
 class CookieVision:
     def __init__(self, debug=False):
@@ -14,28 +13,48 @@ class CookieVision:
         # V é brilho, quanto mais baixo, mais preto
         self.lower_golden = np.array([20, 140, 85]) 
         self.upper_golden = np.array([33, 255, 190])
-
         self.min_opacidade = 235
 
+        self.hwnd = self.encontrar_janela_cookie()
+        self.rect = self.get_window_rect()
+
+    def encontrar_janela_cookie(self):
+    # Busca o identificador (HWND) da janela.
+        def callback(hwnd, hwnds):
+            if win32gui.IsWindowVisible(hwnd):
+                titulo = win32gui.GetWindowText(hwnd)
+                if titulo.endswith("Cookie Clicker"):
+                    hwnds.append(hwnd)
+            return True
+
+        hwnds = []
+        win32gui.EnumWindows(callback, hwnds)
+        return hwnds[0] if hwnds else None
+
     def get_window_rect(self):
-        hwnd = encontrar_janela_cookie()
-        if hwnd:
-            rect = win32gui.GetWindowRect(hwnd)
+
+        if self.hwnd:
+            rect = win32gui.GetWindowRect(self.hwnd)
             return {"top": rect[1], "left": rect[0], "width": rect[2]-rect[0], "height": rect[3]-rect[1]}
         return None
     
-    def get_store_status(self, rect):
+    def get_screenshot(self):
+        if self.rect:
+            screenshot = np.array(self.sct.grab(self.rect))
+            return screenshot       
+
+    def get_store_status(self):
         # largura da loja
-        store_w = int(rect["width"] * 0.17)
-        store_x_start = rect["width"] - store_w
+        store_w = int(self.rect["width"] * 0.17)
+        store_x_start = self.rect["width"] - store_w
         
         # altura da loja
-        offset_y = int(rect["height"] * 0.188)
-        height_loja = rect["height"] - offset_y
+        offset_y = int(self.rect["height"] * 0.188)
+        height_loja = self.rect["height"] - offset_y
 
         raw_store = np.array(self.sct.grab({
-            "top": rect["top"] + offset_y,
-            "left": rect["left"] + store_x_start,
+            "top": self.rect["top"] + offset_y,
+            "left": self.rect["left"] + store_x_start,
             "width": store_w,
             "height": height_loja
         }))
@@ -63,8 +82,6 @@ class CookieVision:
             if self.debug:
                 cor_rect = (0, 255, 0) if pode_comprar else (0, 0, 255)
                 cv2.rectangle(store_debug_img, (0, y_start), (store_w, y_end), cor_rect, 2)
-                cv2.putText(store_debug_img, f"V: {int(brilho_medio)}", (5, y_start + 20), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, cor_rect, 1)
 
             if pode_comprar:
                 local_cX = store_w // 2
@@ -85,17 +102,17 @@ class CookieVision:
         return buyable_coords
     
 
-    def get_upgrade_status(self, rect):
+    def get_upgrade_status(self):
 
-        store_w = int(rect["width"] * 0.17)
-        store_x_start = rect["width"] - store_w
-        
-        upgrade_y_start = int(rect["height"] * 0.085) 
-        upgrade_height = int(rect["height"] * 0.06)
+        store_w = int(self.rect["width"] * 0.17)
+        store_x_start = self.rect["width"] - store_w
+
+        upgrade_y_start = int(self.rect["height"] * 0.085) 
+        upgrade_height = int(self.rect["height"] * 0.06)
 
         raw_upgrades = np.array(self.sct.grab({
-            "top": rect["top"] + upgrade_y_start,
-            "left": rect["left"] + store_x_start,
+            "top": self.rect["top"] + upgrade_y_start,
+            "left": self.rect["left"] + store_x_start,
             "width": store_w,
             "height": upgrade_height
         }))
@@ -113,7 +130,7 @@ class CookieVision:
         if self.debug:
 
             debug_img = v_channel.copy()
-            
+
             local_x = (upgrade_size // 2)
             local_y = (upgrade_size // 2)
             
@@ -136,22 +153,20 @@ class CookieVision:
 
 
     def find_any_golden(self):
-        rect = self.get_window_rect()
-        if not rect: return None
 
-        screenshot = np.array(self.sct.grab(rect))
-        
+        screenshot = self.get_screenshot()
+
         alpha_channel = screenshot[:, :, 3]
         _, alpha_mask = cv2.threshold(alpha_channel, self.min_opacidade, 255, cv2.THRESH_BINARY)
 
         img_bgr = cv2.cvtColor(screenshot, cv2.COLOR_BGRA2BGR)
         hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
         color_mask = cv2.inRange(hsv, self.lower_golden, self.upper_golden)
-        
+
         combined_mask = cv2.bitwise_and(color_mask, alpha_mask)
-        
+
         # Só Deus sabe o que é isso aqui embaixo
-        kernel = np.ones((8, 8), np.uint8) 
+        kernel = np.ones((10, 10), np.uint8) 
         mask = cv2.morphologyEx(combined_mask, cv2.MORPH_CLOSE, kernel)
         mask = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=1)
         
@@ -172,3 +187,55 @@ class CookieVision:
                     cY = int(M["m01"] / M["m00"])
                     return (cX, cY)
         return None
+
+
+    def close_pop_ups(self):
+
+        pop_ups_w = int(self.rect["width"] * 0.17)
+        pop_ups_x_start = self.rect["width"] - pop_ups_w
+
+        pop_ups_y_start = int(self.rect["height"] * 0.085) 
+        pop_ups_height = int(self.rect["height"] * 0.06)
+
+        raw_pop_ups = np.array(self.sct.grab({
+            "top": self.rect["top"] + pop_ups_y_start,
+            "left": self.rect["left"] + pop_ups_x_start,
+            "width": pop_ups_w,
+            "height": pop_ups_height
+        }))
+
+        template = cv2.imread("src/vision/assets/close_popup.png")
+        template_y, template_x = template.shape[:2]
+        img_bgr = cv2.cvtColor(raw_pop_ups, cv2.COLOR_BGRA2BGR)
+
+        result = cv2.matchTemplate(img_bgr, template, cv2.TM_CCOEFF_NORMED)
+
+        threshold = 0.8
+
+        locations = np.where(result >= threshold)
+
+        if self.debug:
+            debug_img = img_bgr.copy()
+        
+        if locations:
+            pass
+        else:
+            return None
+
+        points = []
+
+        for pt in locations:
+            center_y = pt[0] + template_y // 2
+            center_x = pt[1] + template_x // 2
+
+            real_center_y = pop_ups_y_start + center_y - 30 # 30 é compensação da barra da janela do windows
+            real_center_x = pop_ups_x_start + center_x
+            points.append((real_center_x, real_center_y))
+
+            if self.debug:
+                cv2.circle(debug_img, (center_x, center_y), 20, (0, 255, 0), 2)
+
+        if self.debug:
+            cv2.imshow("Debug - Pop-up-killer", debug_img)
+        
+        return points[0]
